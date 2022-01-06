3457545e95d4024e3f9f33dee844aeaa5765438f
linlingaixuexi <78462907+linlingaixuexi@users.noreply.github.com>
2022-01-06 15:30:28 -0600

    Customizing the header by adding a number of links

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -29,9 +29,20 @@ class TodoApp extends Component {
 class HeaderComponent extends Component {
     render() {
         return (
-            <div>
-                Header <hr/>
-            </div>
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href = "http://www.linglin.com" className = "navbar-brand">linglin</a></div> 
+                    <ul className = "navbar-nav">
+                        <li><Link className="nav-link" to = "/welcome/linglin">Home</Link></li>
+                        <li><Link className="nav-link" to = "/todos">Todos</Link></li>
+                    </ul> 
+                    <ul className = "navbar-nav navbar-collapse justify-content-end">
+                        <li><Link className="nav-link" to = "/login">Login</Link></li>
+                        <li><Link className="nav-link" to = "/logout">Logout</Link></li>
+                    </ul> 
+                </nav>
+            </header>
+            
         )
     }
 }
